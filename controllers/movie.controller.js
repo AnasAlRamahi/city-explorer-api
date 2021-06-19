c9dcadf828175e61afd4758c0bf1b177fa9e8edd
Anas Al-Ramahi <anas_alramahi@outlook.com>
2021-06-19 14:04:40 +0300

    added the cache object

--- controllers/movie.controller.js
+++ controllers/movie.controller.js
@@ -2,22 +2,33 @@ const axios = require('axios');
 const Movie = require('../models/movie.model');
 require('dotenv').config();
 
+const Cache = require('../helper/cache');
+const movieObj = new Cache();
 
 const movieController = (req,res) => {
   let cityName = req.query.city;
-  axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_KEY}&query=${cityName}`)
-    .then(response => {
-      const arrOfMovies = [];
-      response.data.results.map(item => {
-        let imageURL = `https://image.tmdb.org/t/p/w500${item.poster_path}`;
-        let movieObject = new Movie(item.title, item.overview, item.vote_average, item.vote_count, imageURL, item.popularity, item.release_date);
-        arrOfMovies.push(movieObject);
+  let requestKey = `movie-${cityName}`;
+
+  if(movieObj[requestKey]){
+    res.json(movieObj[requestKey]);
+    console.log('sent from cache');
+  }else{
+    axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_KEY}&query=${cityName}`)
+      .then(response => {
+        const arrOfMovies = [];
+        response.data.results.map(item => {
+          let imageURL = `https://image.tmdb.org/t/p/w500${item.poster_path}`;
+          let movieObject = new Movie(item.title, item.overview, item.vote_average, item.vote_count, imageURL, item.popularity, item.release_date);
+          arrOfMovies.push(movieObject);
+        });
+        movieObj[requestKey] = arrOfMovies;
+        res.send(arrOfMovies);
+        console.log('sent from axios');
+      })
+      .catch(error => {
+        res.send('Error hahahahahaha  ', error.message);
       });
-      res.send(arrOfMovies);
-    })
-    .catch(error => {
-      res.send('Error hahahahahaha  ', error.message);
-    });
+  }
 };
 
 module.exports = movieController;
--- controllers/weather.controller.js
+++ controllers/weather.controller.js
@@ -2,23 +2,35 @@ const axios = require('axios');
 const Forecast = require('../models/weather.model');
 require('dotenv').config();
 
+const Cache = require('../helper/cache');
+const cacheObj = new Cache();
+
 
 const weatherController = (req,res) => {
   let latitude = req.query.lat;
   let longitude = req.query.lon;
-  axios.get(`https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_KEY}&lat=${latitude}&lon=${longitude}`).then(response => {
-    const arrOfDays = [];
-    response.data.data.map(item => {
-      let description = `Low of ${item.low_temp}, high of ${item.high_temp} with ${item.weather.description}`;
-      let dayWeather = new Forecast(item.valid_date, description);
-      arrOfDays.push(dayWeather);
-    });
-    res.send(arrOfDays);
-  }).catch(error => {
-    res.send('Error hahahahahaha  ', error.message);
-  });
-};
+  let requestKey = `${latitude}-${longitude}`;
 
+  if (cacheObj[requestKey] && (Date.now()-cacheObj[requestKey].timeStamp < 3000)){
+    res.json(cacheObj[requestKey].content);
+    console.log('sent from the cache', cacheObj[requestKey]);
+  }else{
+    axios.get(`https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_KEY}&lat=${latitude}&lon=${longitude}`).then(response => {
+      const content = response.data.data.map(item => {
+        let description = `Low of ${item.low_temp}, high of ${item.high_temp} with ${item.weather.description}`;
+        return new Forecast(item.valid_date, description);
+      });
+      cacheObj[requestKey] = {};
+      cacheObj[requestKey].content = [];
+      cacheObj[requestKey].content = content;
+      cacheObj[requestKey].timeStamp = Date.now();
+      console.log('sent from the axios request',cacheObj[requestKey]);
+      res.send(cacheObj[requestKey].content);
+    }).catch(error => {
+      res.send('Error hahahahahaha  ', error.message);
+    });
+  }
+};
 
 
 module.exports = weatherController;
